8a3b32e71ade83e96c74ae1887109e8a1026b2f6
Harper-kim <mj0708_@naver.com>
2023-10-17 18:13:51 +0900

    test17

--- force-app/main/default/lwc/carlistView/carlistView.js
+++ force-app/main/default/lwc/carlistView/carlistView.js
@@ -1,12 +1,15 @@
-import { LightningElement,track } from 'lwc';
-import TireData from '@salesforce/apex/TireController.TireData';
+import { LightningElement,track,api } from 'lwc';
+import tireData from '@salesforce/apex/TireController.tireData';
 import searchOpp from '@salesforce/apex/TireController.searchOpp';
+import updateProduct from '@salesforce/apex/TireController.updateProduct';
+import getproInfo from '@salesforce/apex/TireController.getproInfo';
 
+const ACTIONS = [{label: 'edit', value: 'edit'}]
 
 const colums = [
     {label: 'Material__c', fieldName: 'Material__c', type: 'text'},
-    {label: 'Desciption__c', fieldName: 'Desciption__c', type: 'text'},
-    {label: 'Desciption2__c', fieldName: 'Desciption2__c', type: 'text'},
+    {label: 'Description__c', fieldName: 'Description__c', type: 'text'},
+    {label: 'Description2__c', fieldName: 'Description2__c', type: 'text'},
     {label: 'Type__c', fieldName: 'Type__c', type: 'text'},
     {label: 'Base_UOM__c', fieldName: 'Base_UOM__c', type: 'text'},
     {label: 'Net_Weight__c', fieldName: 'Net_Weight__c', type: 'text'},
@@ -19,70 +22,59 @@ const colums = [
     {label: 'S_Width__c', fieldName: 'S_Width__c', type: 'text'},
     {label: 'Tire_Type__c', fieldName: 'Tire_Type__c', type: 'text'},
     {label: 'Performance__c', fieldName: 'Performance__c', type: 'text'},
-    {label: 'Name', fieldName: 'link', type: 'url', typeAttributes: { label: {fieldName: 'Name'}}}
-
+    {label: 'Name', fieldName: 'link', type: 'url', typeAttributes: { label: {fieldName: 'Name'}}},
+    {fieldName: 'actions', type: 'action', typeAttributes: {rowActions: ACTIONS}}
 ]
 
 export default class CarlistView extends LightningElement {
+    @api recordId;
+    @api objectName;
+
     @track data;
     colums = colums;
 
-    // connectedCallback(){
-    //     this.TireAll();
-    // }
 
-    // TireAll(){
-    //     TireData()
-    //     .then(result => {
-    //         this.data = result;
-    //     });
-    // }
+    @track isEditModal = false;
 
-    // //검색
-    // handleSearch(event){
-    //     if(!event.target.value){
-    //         this.TireAll();
-    //     }else{
-    //         console.log(event.target.value);
-    //         this.search();
-    //     }
-    // }
-
-    // search(){
-    //     const searchOpps = searchOpp({searchString: event.target.value});
-    //     this.data = searchOpps;
-    //     console.log('searchOPps: ' +searchOpps);
-    // }
 
     //전체데이터 불러오기
     async connectedCallback(){
-        await this.TireAll();
-       }
+        await this.tireAll();
+    }
   
       //전체보기
-      async TireAll(){
-          const result = await TireData();
+      async tireAll(){
+          const result = await tireData();
           this.data = result.map(row => {
-              return this.mapOpps(row);
+              return this.mapProducts(row);
           }) 
       }
 
       //매핑시켜주는 
-     mapOpps(row){
+      mapProducts(row){
         return {...row,
             Name: row.Name,
             link: `/${row.Id}`,
         };
     }
 
+    //edit모달 열기
+    editModalBox(){
+        this.isEditModal = true;
+    }
+    //edit모달창 닫기
+    editCloseModalBox(){
+        this.isEditModal = false;
+    }
+
     //검색
     async handleSearch(event){
-        //이벤트가 발생하지 않았을 때 
-        if(!event.target.value){
-            await this.TireAll();
-        }else{
+        if(event.target.value){
             console.log(event.target.value);
             await this.search();
+        }
+        else{
+            await this.tireAll();
         }
     }
 
@@ -90,10 +82,40 @@ export default class CarlistView extends LightningElement {
     async search(){
         const searchOpps = await searchOpp({searchString: event.target.value});
         this.data = searchOpps.map(row =>{
-            return this.mapOpps(row);
+            return this.mapProducts(row);
         });
     }
 
+    async handleRowAction(event){
+        if(event.detail.action.value == 'edit'){
+            const response = await getproInfo({productId : event.detail.row.Id});
+                this.id = response.Id;
+                this.Name = response.Name;
+                this.Description__c = response.Description__c;
+                this.Description2__c = response.Description2__c;
+            this.editModalBox();
+        }
+    }
+
+    async handleSave(event){
+        const row = event.detail.row;
+        await updateProduct({
+           ProductId : row.Id,
+           Description: row.Description__c,
+           Description2: row.Description2__c
+        });
+        this.editOpps();
+    }
     
+    //편집
+    editOpps(){
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: '기회를 업데이트하였습니다.',
+                variant: 'success'
+            }),);
+            this.editCloseModalBox();
+    }
 
 }
